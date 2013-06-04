03886388c6e35ea57fa868bd3bcb398d0674cbd9
pjohans <pjohans@gmail.com>
2013-06-04 09:56:35 +0200

    refactoring for my_page tabs

---
+++ js/getOpenUserStatus.js
@@ -0,0 +1,11 @@
+(function ($) {
+    Drupal.behaviors.bibdk_openuserstatus = {
+        attach: function(context, settings) {
+            $(document).ready(function(){
+                alert('TESTHSET');
+            });
+        }
+    };
+
+}(jQuery)); 
+
