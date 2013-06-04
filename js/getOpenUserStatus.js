1bf45177d943dbda11ea356e03aa06e30e8f0e15
pjohans <pjohans@gmail.com>
2013-06-04 09:59:00 +0200

    ajax call for userstatus

--- js/getOpenUserStatus.js
+++ js/getOpenUserStatus.js
@@ -1,11 +1,27 @@
 (function ($) {
     Drupal.behaviors.bibdk_openuserstatus = {
-        attach: function(context, settings) {
-            $(document).ready(function(){
-                alert('TESTHSET');
+        attach: function(context) {
+            $('.bibdk-openuserstatus-mypage-info',context).each(function(element){
+                Drupal.bibdkGetUserStatus(element,context);
             });
         }
     };
+    
+    Drupal.setUserStatus = function (data) {
+        $('.bibdk-openuserstatus-mypage-info').html(data.html);
+    },
+    
+    Drupal.bibdkGetUserStatus =  function(element,context) {
+        // save context in global var to reload later
+        Drupal.settings.userStatus = context;
+        // Call ajax 
+        $.ajax({
+            url:Drupal.settings.basePath + 'openuserstatus/getuserstatus',
+            type:'POST',
+            dataType:'json',
+            success:Drupal.setUserStatus
+        }); 
+    }
 
 }(jQuery)); 
 
