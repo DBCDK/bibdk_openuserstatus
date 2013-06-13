4ce6c18805f529677edc54fd69aac4bc14d44719
JrgenGNielsen <jgn@dbc.dk>
2013-06-13 17:29:05 +0200

    format row items: added theme template

--- theme/bibdk_openuserstatus.tpl.php
+++ theme/bibdk_openuserstatus.tpl.php
@@ -1,8 +1,22 @@
-<p>
-  <?php print t('Here goes basic information blah-blah...', array(), array('bibdk_openuserstatus')) ?>
+<p class="userstatus-intro">
+  <?php print t($variables['label_userstatus_intro'], array(), array('context' => 'bibdk_openuserstatus')) ?>
 </p>
 
-<p>Go to:</p>
-<a href="#loans">loans</a>
-<a href="#reservations">reservations</a>
-<a href="#fiscal">fiscal</a>
+<div class="userstatus-menu">
+  <p class="userstatus-menu-label">
+    <?php print t('label_userstatus_go_to', array(), array('context' => 'bibdk_openuserstatus')) ?>
+  </p>
+
+  <p class="userstatus-menu-item">
+    <a href="#loans"><?php print t('label_userstatus_loans', array(), array('context' => 'bibdk_openuserstatus')) ?></a>
+  </p>
+
+  <p class="userstatus-menu-item">
+    <a href="#reservations"><?php print t('label_userstatus_reservations', array(), array('context' => 'bibdk_openuserstatus')) ?></a>
+  </p>
+
+  <p class="userstatus-menu-item">
+    <a href="#fiscal"><?php print t('label_userstatus_fiscal', array(), array('context' => 'bibdk_openuserstatus')) ?></a>
+  </p>
+
+</div>
