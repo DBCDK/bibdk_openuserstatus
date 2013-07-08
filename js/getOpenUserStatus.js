d01e9b3de694500e16e1381bf3a2dbd7cd2fc8ed
Martin Møller <mmj@dbc.dk>
2013-07-08 15:32:47 +0200

    Codeformatting

--- js/getOpenUserStatus.js
+++ js/getOpenUserStatus.js
@@ -1,26 +1,26 @@
-(function ($) {
+(function($) {
     Drupal.behaviors.bibdk_openuserstatus = {
         attach: function(context) {
-            $('.bibdk-openuserstatus-mypage-info',context).each(function(element){
-                Drupal.bibdkGetUserStatus(element,context);
+            $('.bibdk-openuserstatus-mypage-info', context).each(function(element) {
+                Drupal.bibdkGetUserStatus(element, context);
             });
         }
     };
-    
-    Drupal.setUserStatus = function (data) {
+
+    Drupal.setUserStatus = function(data) {
         $('.bibdk-openuserstatus-mypage-info').html(data.html);
-    },
-    
-    Drupal.bibdkGetUserStatus =  function(element,context) {
+    };
+
+    Drupal.bibdkGetUserStatus = function(element, context) {
         // save context in global var to reload later
         Drupal.settings.userStatus = context;
-        // Call ajax 
+        // Call ajax
         $.ajax({
-            url:Drupal.settings.basePath + 'openuserstatus/getuserstatus',
-            type:'POST',
-            dataType:'json',
-            success:Drupal.setUserStatus
-        }); 
+            url: Drupal.settings.basePath + 'openuserstatus/getuserstatus',
+            type: 'POST',
+            dataType: 'json',
+            success: Drupal.setUserStatus
+        });
     }
 
 }(jQuery)); 
